415c1b09ea07967263f3a49ca01b8a5c367ebbbc
Avinash Makhija <avinashmakhija429@gmail.com>
2020-10-02 14:35:46 +0530

    Updateded

---
+++ lib/details_screen.dart
@@ -0,0 +1,314 @@
+import 'package:Sample/constants.dart';
+import 'package:Sample/main.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter/material.dart';
+//import 'package:flutter_svg/flutter_svg.dart';
+import 'package:Sample/LecturesList.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+
+class StoreData extends StatefulWidget {
+  String Course;
+  StoreData(String course_name) {
+    this.Course = course_name;
+  }
+
+  @override
+  _State createState() => _State(Course);
+}
+
+class _State extends State<StoreData> {
+  List<LectureList> ListofLinks = [];
+  String Course;
+  _State(course_name) {
+    this.Course = course_name;
+  }
+  @override
+  void initState() {
+    // print("Har Har Mahadev");
+    final fb = FirebaseDatabase.instance
+        .reference()
+        .child(Course)
+        .child("Lecture List");
+
+    fb.once().then((DataSnapshot snap) {
+      print(snap);
+      int index = 0;
+      String No_of_course;
+      var data = snap.value;
+      // var lecture = snap.value.keys;
+      print(data);
+      ListofLinks.clear();
+
+      data.forEach((key, value) {
+        index = index + 1;
+        No_of_course = (index < 9) ? "0" + "$index" : "$index";
+        ListofLinks.add(new LectureList(No_of_course, value['link'], key));
+      });
+      setState(() {});
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        decoration: BoxDecoration(
+          color: Color(0xFFF5F4EF),
+          image: DecorationImage(
+            image: AssetImage("images/ux_big.png"),
+            alignment: Alignment.topRight,
+          ),
+        ),
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.only(left: 20, top: 50, right: 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      // SvgPicture.asset("icons/arrow-left.svg"),
+                      //SvgPicture.asset("icons/more-vertical.svg"),
+                    ],
+                  ),
+                  SizedBox(height: 30),
+                  ClipPath(
+                    clipper: BestSellerClipper(),
+                    child: Container(
+                      color: kBestSellerColor,
+                      padding: EdgeInsets.only(
+                          left: 10, top: 5, right: 20, bottom: 5),
+                      child: Text(
+                        "10k Students Enrolled...".toUpperCase(),
+                        style: TextStyle(
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 16),
+                  Text("AWS", style: kHeadingextStyle),
+                  SizedBox(height: 16),
+                  SizedBox(height: 20),
+                ],
+              ),
+            ),
+            SizedBox(height: 60),
+            Expanded(
+              child: Container(
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(50),
+                  color: Colors.white,
+                ),
+                child: StaggeredGridView.countBuilder(
+                  crossAxisCount: 1,
+
+                  // _State stat =  new _State(),
+
+                  itemCount: ListofLinks.length,
+                  // crossAxisSpacing: 20,
+                  // mainAxisSpacing: 20,
+                  itemBuilder: (context, index) {
+                    return FlatButton(
+                      materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                      padding: EdgeInsets.all(0),
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) =>
+                                  VideoPlayer(ListofLinks[index].link)),
+                        );
+                      },
+                      child: Column(
+                        children: <Widget>[
+                          Padding(
+                            padding: const EdgeInsets.fromLTRB(30, 0, 30, 0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                // Text("Course Content", style: kTitleTextStyle),
+                                SizedBox(height: 10),
+                                CourseContent(
+                                  number: ListofLinks[index].number,
+                                  // duration: 5.35,
+                                  title: ListofLinks[index].title,
+                                  link: ListofLinks[index].link,
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+
+                  staggeredTileBuilder: (index) => StaggeredTile.fit(1),
+
+                  //
+                ),
+              ),
+            ),
+            //  Expanded(
+            // Container(
+            //   width: double.infinity,
+            //   decoration: BoxDecoration(
+            //     borderRadius: BorderRadius.circular(50),
+            //     color: Colors.white,
+            //   ),
+            //   child: Column(
+            //     children: <Widget>[
+            Positioned(
+              // right: 0,
+              // left: 0,
+              // bottom: 0,
+              child: Container(
+                padding: EdgeInsets.all(20),
+                height: 100,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(40),
+                  boxShadow: [
+                    BoxShadow(
+                      offset: Offset(0, 4),
+                      blurRadius: 50,
+                      color: kTextColor.withOpacity(.1),
+                    ),
+                  ],
+                ),
+                child: FlatButton(
+                  onPressed: () {},
+                  child: Row(
+                    children: <Widget>[
+                      SizedBox(width: 20),
+                      Expanded(
+                        child: Container(
+                          alignment: Alignment.center,
+                          height: 56,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(40),
+                            color: kBlueColor,
+                          ),
+                          child: Text(
+                            "Enroll Now",
+                            style: kSubtitleTextSyule.copyWith(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            //  ],
+            //  ),
+            //  ),
+            // )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class DetailsScreen extends StatelessWidget {
+  String Course_name;
+  DetailsScreen(Name) {
+    this.Course_name = Name;
+  }
+  @override
+  Widget build(BuildContext context) {
+    return StoreData(Course_name);
+  }
+}
+
+class CourseContent extends StatelessWidget {
+  final String number;
+  // final double duration;
+  final String title;
+  final String link;
+  const CourseContent({
+    Key key,
+    this.number,
+    //this.duration,
+    this.title,
+    this.link,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 30),
+      child: Row(
+        children: <Widget>[
+          Text(
+            number,
+            style: kHeadingextStyle.copyWith(
+              color: kTextColor.withOpacity(.15),
+              fontSize: 32,
+            ),
+          ),
+          SizedBox(width: 20),
+          RichText(
+            text: TextSpan(
+              children: [
+                // TextSpan(
+                //   text: link,
+                //   style: TextStyle(
+                //     color: kTextColor.withOpacity(.5),
+                //     fontSize: 18,
+                //   ),
+                // ),
+                TextSpan(
+                  text: title,
+                  style: kSubtitleTextSyule.copyWith(
+                    fontWeight: FontWeight.w600,
+                    height: 1.5,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Spacer(),
+          Container(
+            margin: EdgeInsets.only(left: 20),
+            height: 40,
+            width: 40,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              color: kGreenColor.withOpacity(1),
+            ),
+            child: Icon(Icons.play_arrow, color: Colors.white),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class BestSellerClipper extends CustomClipper<Path> {
+  @override
+  getClip(Size size) {
+    var path = Path();
+    path.lineTo(size.width - 20, 0);
+    path.lineTo(size.width, size.height / 2);
+    path.lineTo(size.width - 20, size.height);
+    path.lineTo(0, size.height);
+    path.lineTo(0, 0);
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper oldClipper) {
+    return false;
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,325 @@
+import 'package:Sample/constants.dart';
+import 'package:Sample/details_screen.dart';
+import 'package:Sample/model/category.dart';
+import 'package:Sample/model/categoryImage.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter/material.dart';
+//import 'package:Sample/model/DashboardCategory.dart';
+import 'package:Sample/Dashboard.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+//import 'package:flutter_svg/flutter_svg.dart';
+import 'package:Sample/chewie_list_item.dart';
+import 'package:video_player/video_player.dart';
+import 'package:Sample/details_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Sample App',
+      theme: ThemeData(),
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  List<Category> Categories = [];
+  void initState() {
+    // print("Har Har Mahadev");
+    final fb = FirebaseDatabase.instance.reference();
+    // DatabaseReference ref = FirebaseDatabase.instance.reference();
+    // ref.child('Computer Network').once().then((DataSnapshot snap) {
+    //   var Lecture = snap.value.keys;
+    //   var Link = snap.value;
+    //   // var i = 1;
+    //   for (var key in Lecture) {
+    //     ListofLinks.add(new LectureList("01", Link[key]['Link'], key));
+    //     // i = i + 1;
+    //   }
+    // });
+    fb.once().then((DataSnapshot snap) {
+      print(snap);
+      var data = snap.value;
+      // var lecture = snap.value.keys;
+      print(data);
+      Categories.clear();
+
+      data.forEach((key, value) {
+        Categories.add(new Category(key, value['image']));
+      });
+      setState(() {});
+    });
+  }
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Charusat E-learn"),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          children: <Widget>[
+            new DrawerHeader(
+              child: Icon(
+                Icons.account_box,
+                size: 90,
+              ),
+              decoration: BoxDecoration(color: Colors.white),
+            ),
+            ListTile(
+              title: Text('Courses Enrolled'),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => Dashboard()),
+                );
+                //Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: Text('Logout'),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => Dashboard()),
+                );
+                //Navigator.pop(context);
+              },
+            ),
+            //Text('Menu Item 1'),
+            //Text('Menu Item 2')
+          ],
+        ),
+      ),
+      body: Padding(
+        padding: EdgeInsets.only(left: 20, top: 50, right: 20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            // Row(
+            //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            //   children: <Widget>[
+            //     //  SvgPicture.asset("icons/menu.svg")
+            //     //  Icon(Icons.menu),
+            //    // Image.asset("images/user.png"),
+            //   ],
+            // ),
+            // SizedBox(height: 30),
+            Text("Hey Avinash,", style: kHeadingextStyle),
+            Text("Find a course you want to learn", style: kSubheadingextStyle),
+            Container(
+              margin: EdgeInsets.symmetric(vertical: 30),
+              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 16),
+              height: 60,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: Color(0xFFF5F5F7),
+                borderRadius: BorderRadius.circular(40),
+              ),
+              child: Row(
+                children: <Widget>[
+                  //  SvgPicture.asset("icons/search.svg"),
+                  // IconButton(
+                  //     icon: Icon(Icons.search),
+                  //     onPressed: () {
+                  //       showSearch(context: context, delegate: SearchBar());
+                  //     }),
+                  // Icons.search;
+                  FlatButton(
+                    onPressed: () {
+                      showSearch(context: context, delegate: SearchBar());
+                    },
+                    child: Row(
+                      children: <Widget>[
+                        Icon(Icons.search),
+                        SizedBox(width: 16),
+                        Text(
+                          "Search for anything",
+                          style: TextStyle(
+                            fontSize: 18,
+                            color: Color(0xFFA0A5BD),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  // Icon(Icons.search),
+                ],
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text("Courses", style: kTitleTextStyle),
+                // Text(
+                //   "See All",
+                //   style: kSubtitleTextSyule.copyWith(color: kBlueColor),
+                // ),
+              ],
+            ),
+            SizedBox(height: 30),
+            Expanded(
+              child: StaggeredGridView.countBuilder(
+                padding: EdgeInsets.all(0),
+                crossAxisCount: 2,
+                itemCount: Categories.length,
+                crossAxisSpacing: 20,
+                mainAxisSpacing: 20,
+                itemBuilder: (context, index) {
+                  return FlatButton(
+                    materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                    padding: EdgeInsets.all(0),
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) =>
+                                DetailsScreen(Categories[index].name)),
+                      );
+                    },
+                    child: Column(
+                      children: <Widget>[
+                        Container(
+                          padding: EdgeInsets.all(20),
+                          //height: index.isEven ? 200 : 240,
+                          height: 200,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(16),
+                            image: DecorationImage(
+                              image: NetworkImage(Categories[index].image),
+                              fit: BoxFit.fill,
+                            ),
+                          ),
+                        ),
+                        Center(
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text(
+                              Categories[index].name,
+                              style: kTitleTextStyle,
+                            ),
+                          ),
+                        ),
+                        // Text(
+                        //   '${categories[index].Instructor}',
+                        //   style: TextStyle(
+                        //     color: kTextColor.withOpacity(.5),
+                        //   ),
+                        // )
+                      ],
+                    ),
+                  );
+                },
+                staggeredTileBuilder: (index) => StaggeredTile.fit(1),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return HomePage();
+  }
+}
+
+class SearchBar extends SearchDelegate<String> {
+  final Courses = [
+    "Python",
+    "Aws",
+    "Computer Networking",
+    "Java",
+    "Operating System"
+  ];
+  @override
+  List<Widget> buildActions(BuildContext context) {
+    // TODO: implement buildActions
+    return [
+      IconButton(
+          icon: Icon(Icons.clear),
+          onPressed: () {
+            query = "";
+          })
+    ];
+  }
+
+  @override
+  Widget buildLeading(BuildContext context) {
+    // TODO: implement buildLeading
+    // throw UnimplementedError();
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    return Center(
+      child: Text(query, style: TextStyle(fontSize: 20)),
+    );
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    // TODO: implement buildSuggestions
+    final my_list = query.isEmpty
+        ? Courses
+        : Courses.where((str) =>
+                str.startsWith(query[0].toUpperCase() + query.substring(1)))
+            .toList();
+    return my_list.isEmpty
+        ? Text("No Course Found....", style: TextStyle(fontSize: 20))
+        : ListView.builder(
+            itemBuilder: (context, index) => ListTile(
+              onTap: () {
+                showResults(context);
+              },
+              title: Text(my_list[index]),
+            ),
+            itemCount: my_list.length,
+          );
+  }
+}
+
+class VideoPlayer extends StatelessWidget {
+  String Link;
+
+  // final String link;
+  VideoPlayer(String link) {
+    Link = link;
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Video Player'),
+      ),
+      body: Container(
+        child: Center(
+          child: ListView(
+            children: <Widget>[
+              ChewieListItem(
+                videoPlayerController: VideoPlayerController.network(
+                  Link,
+                ),
+                looping: true,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
