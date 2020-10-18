de305482dc23ecee695367592f5fc9dfff116452
Avinash Makhija <avinashmakhija429@gmail.com>
2020-10-18 12:58:20 +0530

    Commit Final

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -5,6 +5,7 @@ import 'package:Sample/main.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:get/get.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -147,12 +148,7 @@ class _LoginPageState extends State<LoginPage> {
                               prefs.setString('email', _emailId);
                               print(_emailId);
                               setState(() {
-                                Navigator.pushReplacement(
-                                  context,
-                                  new MaterialPageRoute(
-                                    builder: (context) => HomeScreen(),
-                                  ),
-                                );
+                                Get.off(HomeScreen());
                               });
                             } else {
                               print('Error while Login.');
